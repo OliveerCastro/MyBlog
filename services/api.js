2b5a2c2ab09087ef1ce45ffa7837db9183d3d149
Wagner Castro <102252795+OliveerCastro@users.noreply.github.com>
2022-12-20 17:44:30 -0300

    commit

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://yftpoxnnvskrqtoofwbk.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlmdHBveG5udnNrcnF0b29md2JrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE0NzU3OTAsImV4cCI6MTk4NzA1MTc5MH0.tPlmdpto7kkAySiCd44xQm-VLlrIgBphNa1dHjGwgpE",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlmdHBveG5udnNrcnF0b29md2JrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE0NzU3OTAsImV4cCI6MTk4NzA1MTc5MH0.tPlmdpto7kkAySiCd44xQm-VLlrIgBphNa1dHjGwgpE"
     }
 })
